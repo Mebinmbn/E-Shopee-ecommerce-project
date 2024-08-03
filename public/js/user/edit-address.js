091fa7a4965a6b2981d2e6d02c0f327123f0e872
Mebinmbn <156497822+Mebinmbn@users.noreply.github.com>
2024-08-03 12:55:49 +0530

    Update edit-address.js

--- public/js/user/edit-address.js
+++ public/js/user/edit-address.js
@@ -206,7 +206,7 @@ editForm.addEventListener("submit", function (e) {
 
 editForm.addEventListener(
   "input",
-  debounce((e) => {
+  window.debounce((e) => {
     switch (e.target.id) {
       case "edit-address-fn": // Full name
         checkEditName();
